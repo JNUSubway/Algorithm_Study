55a1531eeecf84a7dab0694d56aaeb008d985821
ambition-kwon <prince5390@gmail.com>
2024-07-03 16:23:20 +0900

    [BOJ] 2557, 9498, 11022, 1000, 10869, 10817, 11653

---
+++ KwonHyeokWon/202407/세 수.py
@@ -0,0 +1,17 @@
+# 문제
+# 세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 세 정수 A, B, C가 공백으로 구분되어 주어진다. (1 ≤ A, B, C ≤ 100)
+
+# 출력
+# 두 번째로 큰 정수를 출력한다.
+
+import sys
+
+def finput():
+    return sys.stdin.readline().rstrip()
+
+array = list(map(int, finput().split()))
+array.sort()
+print(array[1])
